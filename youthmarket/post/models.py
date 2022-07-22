5912c1d7c46650f52cc6bc6c95d6705f048f7cc4
kyunghwan1207 <gkw1207@hufs.ac.kr>
2022-07-22 19:31:04 +0900

    Feat : "내 채팅API 구현"

--- youthmarket/post/models.py
+++ youthmarket/post/models.py
@@ -14,29 +14,30 @@ class Icon(models.Model):
 class School(models.Model):
     idx = models.AutoField(auto_created=True, primary_key=True, verbose_name="idx")
     schoolName = models.CharField(max_length=50, verbose_name="학교이름")
-    addrCode = models.CharField(max_length=256, verbose_name="우편번호") # 우편번호
-    addr = models.CharField(max_length=256, verbose_name="집주소") # 집 주소
-    detailAddr = models.CharField(max_length=256, verbose_name="상세주소") # 상세주소
+    photo = models.ImageField(blank=True, null=True ,upload_to='schools', verbose_name="이미지경로")
+    addrCode = models.CharField(max_length=256, verbose_name="우편번호")
+    addr = models.CharField(max_length=256, verbose_name="학교주소") 
+    detailAddr = models.CharField(max_length=256, verbose_name="상세주소")
     createdDate = models.DateTimeField(auto_now_add=True, verbose_name="생성시간") # School row가 생성된 시간
     updatedDate = models.DateTimeField(blank=True, null=True, verbose_name="수정시간") # 수정된 시간
     def __str__(self):
         return f"{self.idx}_{self.schoolName}"
 class User(models.Model):
     idx = models.AutoField(auto_created=True, primary_key=True, verbose_name="idx")
-    schoolIdx = models.IntegerField(verbose_name="학교idx") # 학교 인덱스
+    schoolIdx = models.ForeignKey(School,to_field="idx", on_delete=models.DO_NOTHING, verbose_name="학교idx")
     userId = models.CharField(max_length=50, null=False, verbose_name="사용자id")
     userPw = models.CharField(max_length=256, null=False ,verbose_name="사용자pw")
     userRePw = models.CharField(max_length=256, null=True, blank=True ,verbose_name="사용자재입력pw")
     userName = models.CharField(max_length=10, null=False, verbose_name="사용자이름")
     photo = models.ImageField(blank=True, null=True ,upload_to='users', verbose_name="이미지경로")
     addrCode = models.CharField(max_length=256, verbose_name="우편번호") # 우편번호
-    addr = models.CharField(max_length=256, verbose_name="집주소") # 집 주소
+    addr = models.CharField(max_length=256, verbose_name="집 주소") # 집 주소
     detailAddr = models.CharField(max_length=256, verbose_name="상세주소") # 상세주소
     createdDate = models.DateTimeField(auto_now_add=True, verbose_name="생성시간") # 아이디 생성된 시간
     updatedDate = models.DateTimeField(blank=True, null=True, verbose_name="수정시간") # 수정된 시간
     status = models.BooleanField(verbose_name="상태" ,default=False) # 로그인 on/off -> True, False
     def __str__(self):
-        return str(self.idx)
+        return f"{self.idx}_{self.userName}"
 # def upload_post_photo(instance):
 #     return f"/post/{instance.idx}.png"
 class Category(models.Model):
@@ -77,8 +78,10 @@ class ChatRoom(models.Model):
     buyerIdx = models.ForeignKey(User, to_field="idx", on_delete=models.DO_NOTHING, verbose_name="방만든이idx")
     # makerIdx 로 하면 반대편을 joinerIdx 로 해야되는대 애매하다..
     # 사는 사람이 채팅방을 만들것이므로 buyerIdx로 하겠음.
-    sellerIdx = models.IntegerField(null=False,verbose_name="판매자idx")
-    message = models.JSONField(verbose_name="메시지내용")
+    # sellerIdx = models.IntegerField(null=False,verbose_name="판매자idx")
+    sellerIdx = models.ForeignKey(User, null=True, blank=True, related_name="seller_idx", to_field="idx", on_delete=models.DO_NOTHING, verbose_name="판매자idx")
+    # tempIdx = models.ForeignKey(User, null=True, blank=True, related_name="temp_idx" ,to_field="idx", on_delete=models.DO_NOTHING, verbose_name="tempIdx")
+    message = models.JSONField(null=True, blank=True, verbose_name="메시지내용")
     createdDate = models.DateTimeField(auto_now_add=True, verbose_name="생성시간") # chatRoom 생성된 시간
     updatedDate = models.DateTimeField(blank=True, null=True, verbose_name="수정시간") # 수정된 시간
     status = models.BooleanField(verbose_name="상태", default=True) # 방사용가능 on/off -> True, False
@@ -104,3 +107,15 @@ class Msg(models.Model):
 
     def last_10_messages():
         return Msg.objects.order_by('-timestamp').all()[:10] # ASC
+
+class Message(models.Model):
+    idx = models.AutoField(auto_created=True, primary_key=True, verbose_name="idx")
+    author = models.ForeignKey(User, related_name="author_message", on_delete=models.CASCADE, verbose_name="저자")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    chatroomIdx = models.ForeignKey(ChatRoom, related_name="chatroom_message", on_delete=models.CASCADE, verbose_name="채팅방idx")
+    def __str__(self):
+        return self.author.userName
+
+    def last_10_messages(chatroomIdx):
+        return Message.objects.filter(chatroomIdx = chatroomIdx).order_by('-timestamp').all()[:10] # ASC
